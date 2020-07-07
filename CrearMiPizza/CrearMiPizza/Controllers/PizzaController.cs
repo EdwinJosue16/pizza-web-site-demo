860324d246708baa8b16712905510d4f548d49cc
Edwin <edwin.brenes.c@gmail.com>
2020-07-06 23:07:23 -0600

    Agregado: bootsrap 4.5 + Model-Controller Pizza (sin funcionalidades) + personalización del sitio web

---
+++ CrearMiPizza/CrearMiPizza/Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrearMiPizza.Controllers
+{
+    public class PizzaController : Controller
+    {
+        // GET: Pizza
+        public ActionResult CrearPizza()
+        {
+            return View();
+        }
+    }
+}
