90e852a4b4244d9db95fa0effb51c63ff3bc2ae6
Edwin <edwin.brenes.c@gmail.com>
2020-07-07 15:53:22 -0600

    Formulario para tamaño de mi pizza, salsa y masa

--- CrearMiPizza/CrearMiPizza/Models/PizzaModel.cs
+++ CrearMiPizza/CrearMiPizza/Models/PizzaModel.cs
@@ -8,5 +8,11 @@ namespace CrearMiPizza.Models
     public class PizzaModel
     {
         public int[] IngredientesId { get; set; }
+        public string Tamanios { get; set; }
+        public string TipoSalsa { get; set; }
+        public string TipoMasa { get; set; }
+        public string DireccionEntrega { get; set; }
+        public string NombreDuenioPizza { get; set;}
+        public string TelefonoEntrega {get; set;}
     }
 }
