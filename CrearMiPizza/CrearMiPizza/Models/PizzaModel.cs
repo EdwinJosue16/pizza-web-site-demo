ef7da6c0d6eb2096a5842828792efb2b3797a264
Edwin <edwin.brenes.c@gmail.com>
2020-07-07 16:33:15 -0600

    Formulario de datos de entrega agregado con validación y funcional

--- CrearMiPizza/CrearMiPizza/Models/PizzaModel.cs
+++ CrearMiPizza/CrearMiPizza/Models/PizzaModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,8 +12,17 @@ namespace CrearMiPizza.Models
         public string Tamanios { get; set; }
         public string TipoSalsa { get; set; }
         public string TipoMasa { get; set; }
+
+        [Required (ErrorMessage ="Es necesario que indique la dirección de entrega")]
+        [Display(Name = "Ingrese la dirección de entrega")]
         public string DireccionEntrega { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que indique su nombre")]
+        [Display(Name ="Ingrese su nombre completo")]
         public string NombreDuenioPizza { get; set;}
+
+        [Required(ErrorMessage = "Es necesario que indique su número de teléfono")]
+        [Display(Name = "Ingrese su número de teléfono")]
         public string TelefonoEntrega {get; set;}
     }
 }
