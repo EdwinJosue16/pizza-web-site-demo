87d94dd8fa46f56bce7ac377cb5f32309bf50d64
Edwin <edwin.brenes.c@gmail.com>
2020-07-08 01:15:56 -0600

    Se cambia el precio a dolares y se corrige seleccion de elementos de la pizza (parametro erroneo en vista Generar informe)

--- CrearMiPizza/CrearMiPizza/Models/DetallesIngredientesModel.cs
+++ CrearMiPizza/CrearMiPizza/Models/DetallesIngredientesModel.cs
@@ -5,13 +5,13 @@ namespace CrearMiPizza.Models
     {
 
         public string[] NombresIngredientes = new[] { "Jamón", "Hongos", "Salami", "Peperoni", "Pollo", "Carne molida", "Chorizo", "Tosino", "Queso parmesano", "Queso cheddar", "Cebolla", "Jalapeños" };
-        public int[] PreciosIngredientes = new[] { 200, 100, 200, 200, 300, 100, 100, 500, 250, 250, 50, 50 };
+        public double[] PreciosIngredientes = new[] { 0.35, 0.17, 0.35, 0.35, 0.51, 0.17, 0.17, 0.85, 0.43, 0.43, 0.09, 0.09 };
 
         public string [] NombresSalsas = new[] { "Salsa de la casa","Salsa tomate artesanal", "Salsa barbacoa"};
-        public int[] PrecioSalsas = new[] { 250,300,200};
+        public double[] PrecioSalsas = new[] { 0.43,0.51,0.35};
 
         public string[] NombresMasas = new[] { "Original", "Orilla con queso", "Sartén" };
-        public int[] PrecioMasas = new[] { 300, 550, 700 };
+        public double[] PrecioMasas = new[] { 0.51, 0.94, 1.20 };
 
         public string[] Tamanios = new[] { "Personal (4 slides)", "Mediana (8 slides)", "Grande (16 slides)" };
         public int[] IncrementoPrecioXTamanio = new[] { 1, 2, 4};
