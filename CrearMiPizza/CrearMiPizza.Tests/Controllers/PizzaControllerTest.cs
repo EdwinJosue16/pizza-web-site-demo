3493a9706459f7b796e767787623756cec73c147
Edwin <edwin.brenes.c@gmail.com>
2020-07-08 18:32:59 -0600

    Agregación de unit test

---
+++ CrearMiPizza/CrearMiPizza.Tests/Controllers/PizzaControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrearMiPizza.Controllers;
+using System.Web.Mvc;
+using CrearMiPizza.Models;
+
+namespace CrearMiPizza.Tests.Controllers
+{
+    [TestClass]
+    public class PizzaControllerTest
+    {
+        [TestMethod]
+        public void CrearPizza_CorrectoFuncionamiento_Vista()
+        {
+            // Arrange
+            PizzaController controller = new PizzaController();
+
+            // Act
+            ViewResult result = controller.CrearPizza() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GenerarInformePedido_ModeloValido_Vista()
+        {
+            // Arrange
+            PizzaController controller = new PizzaController();
+            PizzaModel pizza = new PizzaModel();
+            pizza.CiudadEntrega = "algo";
+            pizza.DireccionEntrega = "algo";
+            pizza.Tamanio = 1;
+            pizza.TipoMasa = 1;
+            pizza.TipoSalsa = 1;
+            pizza.NombreDuenioPizza = "nombre";
+            pizza.TelefonoEntrega = "algo";
+            // Act
+            ViewResult result = controller.GenerarInformePedido(pizza) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void GenerarInformePedido_ModeloSinValores_Vista()
+        {
+            // Arrange
+            PizzaController controller = new PizzaController();
+            PizzaModel pizza = new PizzaModel();
+            ViewResult result = controller.GenerarInformePedido(pizza) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
